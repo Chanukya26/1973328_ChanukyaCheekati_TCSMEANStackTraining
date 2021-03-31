606719ac1fcc01d71b12c44580d6a15a54fb520e
Chanukya <cheekati.4@wright.edu>
2021-03-30 20:34:40 -0400

    submission 6

---
+++ SampleQuizApplication/SimpleQuizApplication/src/data/javascript.data.ts
@@ -0,0 +1,374 @@
+export const JAVASCRIPT = {
+  id: 1,
+  name: "JavaScript Quiz",
+  description:
+    "JavaScript Quiz (Basic Multiple Choice Questions for JavaScript Developers)",
+  questions: [
+    {
+      id: 1010,
+      name: "C++ was originally developed by--",
+      questionTypeId: 1,
+      options: [
+        {
+          id: 1055,
+          questionId: 1010,
+          name: "Clocksin and Mellish",
+          isAnswer: false
+        },
+        {
+          id: 1056,
+          questionId: 1010,
+          name: "Donald E. Knuth",
+          isAnswer: true
+        },
+        {
+          id: 1057,
+          questionId: 1010,
+          name: "Sir Richard Hadlee",
+          isAnswer: false
+        },
+        {
+          id: 1058,
+          questionId: 1010,
+          name: "None of the above",
+          isAnswer: false
+        }
+      ],
+      questionType: {
+        id: 1,
+        name: "Multiple Choice",
+        isActive: true
+      }
+    },
+    {
+      id: 1011,
+      name: "Which of the followinig are procedural languages?  ",
+      questionTypeId: 1,
+      options: [
+        {
+          id: 1055,
+          questionId: 1010,
+          name: "Pascal",
+          isAnswer: false
+        },
+        {
+          id: 1057,
+          questionId: 1010,
+          name: "loop",
+          isAnswer: false
+        },
+        {
+          id: 1056,
+          questionId: 1010,
+          name: "forEach",
+          isAnswer: true
+        },
+        {
+          id: 1058,
+          questionId: 1010,
+          name: "Smalltalk",
+          isAnswer: false
+        }
+      ],
+      questionType: {
+        id: 1,
+        name: "Multiple Choice",
+        isActive: true
+      }
+    },
+    {
+      id: 1012,
+      name: "What is the difference between let and var?",
+      questionTypeId: 1,
+      options: [
+        {
+          id: 1055,
+          questionId: 1010,
+          name: "let has local scope",
+          isAnswer: true
+        },
+        {
+          id: 1057,
+          questionId: 1010,
+          name: "Both are same",
+          isAnswer: false
+        },
+        {
+          id: 1056,
+          questionId: 1010,
+          name: "var is new data type",
+          isAnswer: false
+        },
+        {
+          id: 1058,
+          questionId: 1010,
+          name: "let consumes more cpu and ram",
+          isAnswer: false
+        }
+      ],
+      questionType: {
+        id: 1,
+        name: "Multiple Choice",
+        isActive: true
+      }
+    },
+    {
+      id: 1013,
+      name: "What is TypeScript?",
+      questionTypeId: 1,
+      options: [
+        {
+          id: 1055,
+          questionId: 1010,
+          name: "A Language based on Javascript",
+          isAnswer: true
+        },
+        {
+          id: 1057,
+          questionId: 1010,
+          name: "script that runs on browser",
+          isAnswer: false
+        },
+        {
+          id: 1056,
+          questionId: 1010,
+          name: "A DataType Collection of Javascript",
+          isAnswer: false
+        },
+        {
+          id: 1058,
+          questionId: 1010,
+          name: "None of the above",
+          isAnswer: false
+        }
+      ],
+      questionType: {
+        id: 1,
+        name: "Multiple Choice",
+        isActive: true
+      }
+    },
+    {
+      id: 1014,
+      name: "Which of the following is right syntex for arrow function?",
+      questionTypeId: 1,
+      options: [
+        {
+          id: 1055,
+          questionId: 1010,
+          name: "a -> { return b; }",
+          isAnswer: false
+        },
+        {
+          id: 1057,
+          questionId: 1010,
+          name: "x <= x + y;",
+          isAnswer: false
+        },
+        {
+          id: 1056,
+          questionId: 1010,
+          name: "x <- x + 5;",
+          isAnswer: false
+        },
+        {
+          id: 1058,
+          questionId: 1010,
+          name: "x => x + 5;",
+          isAnswer: true
+        }
+      ],
+      questionType: {
+        id: 1,
+        name: "Multiple Choice",
+        isActive: true
+      }
+    },
+    {
+      id: 1015,
+      name:
+        "Which new ES6 syntax helps with formatting output text - mixing variables with string literals, for example.",
+      questionTypeId: 1,
+      options: [
+        {
+          id: 1055,
+          questionId: 1010,
+          name: "Generator Functions",
+          isAnswer: false
+        },
+        {
+          id: 1057,
+          questionId: 1010,
+          name: "Arrow Functions",
+          isAnswer: false
+        },
+        {
+          id: 1056,
+          questionId: 1010,
+          name: "Template Strings",
+          isAnswer: true
+        },
+        {
+          id: 1058,
+          questionId: 1010,
+          name: "Set Data Structure",
+          isAnswer: false
+        }
+      ],
+      questionType: {
+        id: 1,
+        name: "Multiple Choice",
+        isActive: true
+      }
+    },
+    {
+      id: 1016,
+      name: "Choose the correct remarks. ",
+      questionTypeId: 1,
+      options: [
+        {
+          id: 1055,
+          questionId: 1010,
+          name: "C++ allows any operator to be overloaded.",
+
+          isAnswer: false
+        },
+        {
+          id: 1056,
+          questionId: 1010,
+          name: "Some of the existing operators cannot be overloaded.",
+
+          isAnswer: true
+        },
+        {
+          id: 1057,
+          questionId: 1010,
+          name: "Operator precedence cannot be changed.",
+          isAnswer: false
+        },
+        {
+          id: 1058,
+          questionId: 1010,
+          name: "All of the above.",
+          isAnswer: false
+        }
+      ],
+      questionType: {
+        id: 1,
+        name: "Multiple Choice",
+        isActive: true
+      }
+    },
+    {
+      id: 1017,
+      name:
+        "What is the difference between == and === ?Which of the following cannot be declared static?",
+
+      questionTypeId: 1,
+      options: [
+        {
+          id: 1055,
+          questionId: 1010,
+          name: "Class",
+          isAnswer: false
+        },
+        {
+          id: 1056,
+          questionId: 1010,
+          name: "Functions",
+          isAnswer: true
+        },
+        {
+          id: 1057,
+          questionId: 1010,
+          name: "Object",
+          isAnswer: false
+        },
+        {
+          id: 1058,
+          questionId: 1010,
+          name: "Both (a) & (b)",
+
+          isAnswer: false
+        }
+      ],
+      questionType: {
+        id: 1,
+        name: "Multiple Choice",
+        isActive: true
+      }
+    },
+    {
+      id: 1018,
+      name: "Which of the following is NOT the method of an Array?",
+      questionTypeId: 1,
+      options: [
+        {
+          id: 1055,
+          questionId: 1010,
+          name: ".map()",
+          isAnswer: false
+        },
+        {
+          id: 1057,
+          questionId: 1010,
+          name: ".includes()",
+          isAnswer: false
+        },
+        {
+          id: 1056,
+          questionId: 1010,
+          name: ".subscribe()",
+          isAnswer: true
+        },
+        {
+          id: 1058,
+          questionId: 1010,
+          name: ".flatMap()",
+          isAnswer: false
+        }
+      ],
+      questionType: {
+        id: 1,
+        name: "Multiple Choice",
+        isActive: true
+      }
+    },
+    {
+      id: 1019,
+      name: "Please Select All the Object Oriented Programming Languages:",
+      questionTypeId: 1,
+      options: [
+        {
+          id: 1055,
+          questionId: 1010,
+          name: "JAVA",
+          isAnswer: true
+        },
+        {
+          id: 1056,
+          questionId: 1010,
+          name: "C",
+          isAnswer: false
+        },
+        {
+          id: 1057,
+          questionId: 1010,
+          name: "C++",
+          isAnswer: false
+        },
+        {
+          id: 1058,
+          questionId: 1010,
+          name: "Allthe Above",
+          isAnswer: false
+        }
+      ],
+      questionType: {
+        id: 1,
+        name: "Multiple Choice",
+        isActive: true
+      }
+    }
+  ]
+};
